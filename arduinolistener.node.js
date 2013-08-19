1867b60522a8a55842ec18964153f79167a1d440
don undeen <don.undeen@metmuseum.org>
2013-08-19 19:03:32 -0400

    now node talking to arduino! this will be the brain that handles communication with Max/MSP

--- arduinolistener.node.js
+++ arduinolistener.node.js
@@ -1,3 +1,68 @@
 // placeholder for the work i'll do getting node and arduino to talk:
-
+// https://github.com/voodootikigod/node-serialport
 //
+var portname = "/dev/cu.usbmodem1411";
+
+
+
+
+var serialport = require("serialport");
+var SerialPort  = serialport.SerialPort;
+var serialPort = new SerialPort(portname, {
+  baudrate: 9600,
+  parser: serialport.parsers.readline("\n") 
+});
+
+
+var fullMessage = "";
+var waitingOnNumber = false;
+
+
+serialPort.on("open", function () {
+  console.log('open');
+  serialPort.on('data', function(data) {
+  	data = data.toString().trim();
+  	var done = false;
+    if(data != ""){
+	 //   console.log('data received: ' + data);
+	    if(/^[A-K][0-9]+$/.test(data)){
+	    	fullMessage = data;
+	    	done = true;
+	    	waitingOnNumber = false;
+	    }else{
+	    	console.log("don't understand " + data);
+	    }
+	    if(done){
+	   // 	console.log("final signal" + fullMessage);
+	    	done = false;
+	    	processMessage(fullMessage);
+		}
+    }
+  });  
+  serialPort.write("ls\n", function(err, results) {
+    console.log('err ' + err);
+    console.log('results ' + results);
+  });  
+});
+
+
+
+function processMessage(command){
+	console.log("going to process command : " + command);
+
+}
+
+
+
+
+/*
+To get list of available ports (selectable from MAX?)
+serialport.list(function (err, ports) {
+  ports.forEach(function(port) {
+    console.log(port.comName);
+    console.log(port.pnpId);
+    console.log(port.manufacturer);
+  });
+});
+
+*/
