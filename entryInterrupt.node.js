6e80265482bcc9fb755145f02302bf52fc4ec5a8
don undeen <donundeen@yahoo.com>
2017-09-12 21:33:51 -0400

    debug

--- entryInterrupt.node.js
+++ entryInterrupt.node.js
@@ -28,8 +28,8 @@ console.log("interrupting");
 		console.log("*******");
                 console.log(currentTrack);
 		console.log("goign to resume");
-//		mopidy.playback.play(currentTrack).then(function(){
-		mopidy.playback.play(track.tl_track).then(function(){
+		mopidy.playback.play(currentTrack).then(function(){
+//		mopidy.playback.play(track.tl_track).then(function(){
 			console.log("just played");
 			mopidy.playback.pause().then(function(){
 				console.log("just paused");
