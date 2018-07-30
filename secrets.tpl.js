d422df3475d60d9f2657ab76f5c018f637ee94c0
don undeen <donundeen@gmail.com>
2018-07-30 17:40:21 -0400

    new file, holds secret values

---
+++ secrets.tpl.js
@@ -0,0 +1,14 @@
+/*
+code secrets. copy to secrets.js, modify values, but don't commit.
+*/
+function secrets(){
+
+	var secrets = { 
+		aio_key : ""
+	};
+
+	return secrets;
+
+}
+
+module.exports.secrets = secrets;
