fb76a74b56f7ada09d31910dcf6ea6d8dee9b4e1
don undeen <donundeen@gmail.com>
2018-08-08 17:13:25 -0400

    debugging parsing body for doorbell

--- arduinolistener.rpi.node.js
+++ arduinolistener.rpi.node.js
@@ -202,7 +202,14 @@ function poll_doorbell(callback){
         console.log("ERROR" + error);
         return;
     }
-    var data = JSON.parse(body);
+    var data = false;
+    try{ 
+    	data = JSON.parse(body);
+    }catch(e){
+	console.log("error parsing doorbell AIO body");
+	console.log(e);
+	console.log(body);
+    }
     var stream = false;
     if(!data.feeds){
 	    console.log("no feeds returned");
