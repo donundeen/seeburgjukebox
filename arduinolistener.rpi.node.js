237e2b1640ae5dd9a2ec22d7278af399bca59b27
don undeen <donundeen@gmail.com>
2019-10-10 18:44:33 -0400

    debug

--- arduinolistener.rpi.node.js
+++ arduinolistener.rpi.node.js
@@ -405,7 +405,7 @@ mopidy.on("state:online", function(){
     setVolumeLow();
 });
 
-//mopidy.on(console.log.bind(console));
+mopidy.on(console.log.bind(console));
 
 mopidy.on("event:trackPlaybackStarted", function(track){
 	console.log("track playback started 2");
